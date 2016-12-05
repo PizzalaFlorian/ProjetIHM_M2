cdbecff3f2848e6e6c0be0cab4eae7a13a87e1bb
PizzalaFlorian <florian.pizzala@gmail.com>
2016-12-05 13:29:01 +0100

    recherche in progress

--- assets/js/recherche.js
+++ assets/js/recherche.js
@@ -1,39 +1,45 @@
 
-// Used to toggle the menu on small screens when clicking on the menu button
-function myFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else { 
-        x.className = x.className.replace(" w3-show", "");
-    }
-}
-
 $(document).ready(function () {
 
-    $("div.w3-top div#nav-inscription").hide();
-    $("div.w3-top div#nav-login").hide();
+    $("#chevalResearch").hide();
+    $("#organismeResearch").hide();
+    $("#eventResearch").hide();
+    $("#advanced").hide();
 
-    $("div ul li#loginButton").click(function () {
-        if( $("div.w3-top div#nav-login").is(":visible")) {
-            $("div.w3-top div#nav-login").hide();
-        }
-        else {
-            $("div.w3-top div#nav-login").show();
-            $("div.w3-top div#nav-inscription").hide();
-        }
-
+    $("#cheval").click(function()
+    { 
+        $("#chevalResearch").show();
+        $("#organismeResearch").hide();
+        $("#eventResearch").hide();
+    });
+    $("#orga").click(function()
+    { 
+        $("#chevalResearch").hide();
+        $("#organismeResearch").show();
+        $("#eventResearch").hide();
+    });
+    $("#event").click(function()
+    { 
+        $("#chevalResearch").hide();
+        $("#organismeResearch").hide();
+        $("#eventResearch").show();
     });
 
-    $("div ul li#InscriptionButton").click(function () {
-        if( $("div.w3-top div#nav-inscription").is(":visible")) {
-            $("div.w3-top div#nav-inscription").hide();
-        }
-        else {
-            $("div.w3-top div#nav-inscription").show();
-            $("div.w3-top div#nav-login").hide();
-        }
-
+    $("#toogle").click(function(e)
+    { 
+        e.preventDefault();
+        $("#advanced").show();
     });
+  
+    // $("div ul li#InscriptionButton").click(function () {
+    //     if( $("div.w3-top div#nav-inscription").is(":visible")) {
+    //         $("div.w3-top div#nav-inscription").hide();
+    //     }
+    //     else {
+    //         $("div.w3-top div#nav-inscription").show();
+    //         $("div.w3-top div#nav-login").hide();
+    //     }
+
+    // });
 
 });
