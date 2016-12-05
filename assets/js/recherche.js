16b3ba7915462559d1d16c372d13a1ad2ce3ceec
PizzalaFlorian <florian.pizzala@gmail.com>
2016-12-05 14:25:12 +0100

    lasttt

--- assets/js/recherche.js
+++ assets/js/recherche.js
@@ -1,36 +1,31 @@
-
-$(document).ready(function () {
+$(document).ready(function() {
 
     $("#chevalResearch").hide();
     $("#organismeResearch").hide();
     $("#eventResearch").hide();
     $("#advanced").hide();
 
-    $("#cheval").click(function()
-    { 
+    $("#cheval").click(function() {
         $("#chevalResearch").show();
         $("#organismeResearch").hide();
         $("#eventResearch").hide();
     });
-    $("#orga").click(function()
-    { 
+    $("#orga").click(function() {
         $("#chevalResearch").hide();
         $("#organismeResearch").show();
         $("#eventResearch").hide();
     });
-    $("#event").click(function()
-    { 
+    $("#event").click(function() {
         $("#chevalResearch").hide();
         $("#organismeResearch").hide();
         $("#eventResearch").show();
     });
 
-    $("#toogle").click(function(e)
-    { 
+    $("#toogle").click(function(e) {
         e.preventDefault();
         $("#advanced").show();
     });
-  
+
     // $("div ul li#InscriptionButton").click(function () {
     //     if( $("div.w3-top div#nav-inscription").is(":visible")) {
     //         $("div.w3-top div#nav-inscription").hide();
