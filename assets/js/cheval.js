ac3674022066ef011117c82fda119bfed93c5337
PizzalaFlorian <florian.pizzala@gmail.com>
2016-12-07 15:51:19 +0100

    POULET

--- assets/js/cheval.js
+++ assets/js/cheval.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
 
     var toogle = true;
+    var papa = true;
+    var mama = true;
+    var momo = true;
 
     $("#formAjoutCheval").hide();
     $("#saillieFormAjout").hide();
+    $("#papaOuté").hide();
+    $("#mamaOuté").hide();
+    $("#momoOuté").hide();
 
     $("#openFormCheval").click(function(e) {
         e.preventDefault();
@@ -15,7 +21,42 @@ $(document).ready(function() {
              $("#formAjoutCheval").hide();
              toogle = true;
         }
-        
+    });
+
+    $("#papa").click(function(e) {
+        e.preventDefault();
+        if(papa){
+            $("#papaOuté").show();
+            papa = false;
+        }
+        else{
+             $("#papaOuté").hide();
+             papa = true;
+        }
+    });
+
+    $("#maman").click(function(e) {
+        e.preventDefault();
+        if(mama){
+            $("#mamaOuté").show();
+            mama = false;
+        }
+        else{
+             $("#mamaOuté").hide();
+             mama = true;
+        }
+    });
+
+     $("#morveux").click(function(e) {
+        e.preventDefault();
+        if(momo){
+            $("#momoOuté").show();
+            momo = false;
+        }
+        else{
+             $("#momoOuté").hide();
+             momo = true;
+        }
     });
 
     $("#oui").click(function(e) {
