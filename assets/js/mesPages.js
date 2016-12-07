b6fad8b91a864c263e121227bdca11c4231600e3
AlexandreUPMF <gannouchedu86@gmail.com>
2016-12-07 15:51:51 +0100

    Saillie
    Fin fiche sur les saillies

---
+++ assets/js/mesPages.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $("div#formAjoutOrga").hide();
+
+    $("a#openFormOrga").click(function (e) {
+        e.preventDefault();
+        if( $("div#formAjoutOrga").is(":visible")) {
+            $("div#formAjoutOrga").hide();
+        }
+        else {
+            $("div#formAjoutOrga").show();
+        }
+
+    });
+
+});
