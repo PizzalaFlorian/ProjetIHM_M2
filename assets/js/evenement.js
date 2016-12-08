d69449275327f8e89db827b16ba59e2c73eedd6f
AlexandreUPMF <gannouchedu86@gmail.com>
2016-12-08 16:25:24 +0100

    Enfin finit !!!
    Ajout pour l'envoie des invitations et changement de follow en suivre !

--- assets/js/evenement.js
+++ assets/js/evenement.js
@@ -8,16 +8,28 @@ $(document).ready(function() {
     $("#loginButton").hide();
     $("#InscriptionButton").hide();
     $("#iconNotif").show();
+    $("div#envoyerInvit").show();
 
 
     $("a#openFormEven").click(function (e) {
         e.preventDefault();
         if( $("div#formAjoutEven").is(":visible")) {
             $("div#formAjoutEven").hide();
+            $("div#envoyerInvit").hide();
         }
         else {
             $("div#formAjoutEven").show();
+            $("div#envoyerInvit").show();
         }
+    });
+
+    $("button#reset").click(function (e) {
+        e.preventDefault();
+        $("div#formAjoutEven").hide();
+    });
+
+    $("div#envoyerInvit a").click(function () {
+        $("div#envoyerInvit").hide();
     });
 
 
